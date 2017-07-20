f77ba97098b84ea1c529d08aba92ee4c2f15ce1b
Q365856 <Q365856@LMUC596320.europe.bmw.corp>
2017-07-20 15:36:18 +0200

    Added new method

--- src/VitusStringFormatter.java
+++ src/VitusStringFormatter.java
@@ -13,4 +13,15 @@ public class VitusStringFormatter {
 		}
 		return result.toString();
 	}
+
+	public String getNicePaddedString(final char padchar, final char delimiter) {
+		StringBuilder result = new StringBuilder(this.length);
+		result.append(delimiter);
+
+		for (int i = 0; i < this.length - 2; i++) {
+			result.append(padchar);
+		}
+		result.append(delimiter);
+		return result.toString();
+	}
 }
