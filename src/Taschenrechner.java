e969dc2388f285d64e178244ca3a0fbe6e69eb79
Q365856 <Q365856@LMUC596320.europe.bmw.corp>
2017-07-20 14:39:22 +0200

    Formatierung Aus- und Eingabe

--- src/Taschenrechner.java
+++ src/Taschenrechner.java
@@ -5,36 +5,47 @@ public class Taschenrechner {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Bitte Operator eingeben: (+ -)");
+		System.out.println("+-----------------------------------+");
+		System.out.println("|            Taschenrechner         |");
+		System.out.println("+-----------------------------------+");
+		System.out.print("| Bitte geben Sie den Operator ein: ");
 		String operator = scanner.nextLine();
-		System.out.println(operator);
-
-		System.out.println("Bitte eine Zahl eingeben: ");
+		System.out.println("+-----------------------------------+");
+		System.out.print("| Bitte eine Zahl eingeben:          ");
 		int op1 = scanner.nextInt();
-		System.out.println(op1);
+		System.out.println("+-----------------------------------+");
 
-		System.out.println("Bitte zweite Zahl eingeben: ");
+		System.out.print("| Bitte zweite Zahl eingeben:       ");
 		int op2 = scanner.nextInt();
-		System.out.println(op2);
+		System.out.println("+-----------------------------------+");
 
 		Taschenrechner taschenrechner = new Taschenrechner();
 
 		switch (operator) {
 		case "+":
-			System.out.println("Add");
-			System.out.printf("Ausgabe: %d \n", taschenrechner.addieren(op1, op2));
+			System.out.println("|            Addition               |");
+			System.out.printf("Ergebnis: %d \n", taschenrechner.addieren(op1, op2));
+			System.out.println("+-----------------------------------+");
 			break;
 		case "-":
-			System.out.println("Sub");
-			System.out.printf("Ausgabe: %d \n", taschenrechner.substrahieren(op1, op2));
+			System.out.println("|           Subtraktion             |");
+			System.out.printf("Ergebnis: %d \n", taschenrechner.substrahieren(op1, op2));
+			System.out.println("+-----------------------------------+");
 			break;
 		case "*":
-			System.out.println("Multi");
-			System.out.printf("Ausgabe: %d \n", taschenrechner.multiplizieren(op1, op2));
+			System.out.println("|         Multiplikation            |");
+			System.out.printf("Ergebnis: %d \n", taschenrechner.multiplizieren(op1, op2));
+			System.out.println("+-----------------------------------+");
+			break;
+		case "/":
+			System.out.println("|            Division               |");
+			System.out.printf("Ergebnis: %d \n", taschenrechner.dividieren(op1, op2));
+			System.out.println("+-----------------------------------+");
 			break;
 
 		default:
-			System.out.println("Geht nicht.");
+			System.out.println("Sie haben keinen Operator eingegeben.");
+			System.out.println("Programm wird geschlossen.");
 			break;
 		}
 
@@ -45,40 +56,14 @@ public class Taschenrechner {
 		return ergebnis;
 	}
 
-	private static double potentieren(double basis, double exponent) {
-		// throw new UnsupportedOperationException("Sorry, den Mist habe ich noch nicht
-		// programmieren lassen. "); //To change body of generated methods, choose Tools
-		// | Templates.
-		double ergebnis = Math.pow(basis, exponent);
-		return ergebnis;
-	}
-
 	public int substrahieren(int a, int b) {
 		int ergebnis = a - b;
 		return ergebnis;
 	}
 
-	private double multiplizieren(long a, float b) {
-		// Implizite Typumwandlung
-		double ergebnis = a * b; // long * float = double
-		return ergebnis;
-	}
-
-	private double multiplizieren(int a, int b) {
+	public double multiplizieren(int a, int b) {
 		double ergebnis = a * b;
 		return ergebnis;
-	}
-
-	private double multiplizieren(int a, float b) {
-		// Mit Hilfe von 'casting' können wir einen
-		// Zahlenwert von einem Datentyp in einen
-		// zweiten "explizit" umwandeln.
-		double ergebnis = multiplizieren((double) a, (double) b);
-		return ergebnis;
-	}
-
-	private double multiplizieren(double a, double b) {
-		return a * b;
 	}
 
 	public double dividieren(double a, double b) {
